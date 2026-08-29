c20eff9a4deecd1d0608272158f41d116a5fe105
Ayush Anupam <160269556+Ayushanupam7@users.noreply.github.com>
2026-08-29 21:24:55 +0530

    Time: 12 ms (25.28%) | Memory: 21.2 MB (47.38%) - LeetSync

---
+++ 169-majority-element/majority-element.py
@@ -0,0 +1,14 @@
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        count = {}  # har number ki frequency store karega
+
+        for i in range(len(nums)):
+            # check kar rahe hain ki current number dictionary me hai ya nahi
+            if nums[i] in count:
+                count[nums[i]] += 1  # already hai, to count badhao
+            else:
+                count[nums[i]] = 1   # pehli baar mila, count 1
+
+            # majority element n/2 se zyada baar hona chahiye
+            if count[nums[i]] > len(nums) / 2:
+                return nums[i]
