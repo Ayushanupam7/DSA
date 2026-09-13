585e6e25ebf74efdae8f620eb323932782cf0636
Ayush Anupam <160269556+Ayushanupam7@users.noreply.github.com>
2026-09-13 23:25:47 +0530

    Time: 0 ms (100%), Space: 19.3 MB (55.8%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.py
+++ 0069-sqrtx/0069-sqrtx.py
@@ -1,14 +1,30 @@
 class Solution:
     def mySqrt(self, x: int) -> int:
+
         if x == 0:
             return 0
-        left, right = 1, x
+
+        left = 1
+        right = x
         ans = 0
+
         while left <= right:
+
+            # Middle number check karenge
             mid = (left + right) // 2
+
+            # Agar mid ka square x se chhota ya equal hai,
+            # to mid ek possible answer hai
             if mid * mid <= x:
-                ans = mid      # mid is a valid candidate
-                left = mid + 1 # try for something bigger
+                ans = mid
+
+                # Answer mil gaya, ab isse bada number try karo
+                left = mid + 1
+
             else:
+                # mid bahut bada hai,
+                # isliye right side ko chhota karo
                 right = mid - 1
+
+        # Last valid answer return karo
         return ans
